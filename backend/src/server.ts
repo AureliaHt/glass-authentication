f636f8d7c4f8597ccfed292baf3385d5c44336b6
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-09-08 15:10:57 +0200

    connection to MongoDB

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 require ('dotenv').config({path: '../config/.env'});
 import express from "express";
 import cors from "cors";
+const mongoose = require('mongoose');
 
 /* import the mongoose models */
 const { List } = require('./database/models/list.model');
@@ -8,6 +9,7 @@ const { Task } = require('./database/models/task.model');
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(cors({
     credentials:true,
@@ -18,12 +20,14 @@ app.use(cors({
 
 /* purpose : get all the lists in the database */
 app.get('/lists', (req, res) => {
-    
+    List.find({}).then((lists:string[]) => {
+        res.send(lists);
+    })
 });
 
 /* purpose : create a new list with an id */
 app.post('/lists', (req, res) => {
-
+    let title = req.body.title;
 });
 
 /* purpose: update a specified list */
@@ -35,6 +39,11 @@ app.patch('/lists/:id', (req,res) => {
 app.delete('/lists:id', (req, res) => {
 
 });
+
+const connectionUrl = ('mongodb+srv://' + process.env.DB_USER_PASS + '@cluster0.bru98.mongodb.net/Task-Manager');
+mongoose.connect(connectionUrl, {useUnifiedTopology: true})
+    .then(() => console.log('connected to MongoDB'))
+    .catch((err:any) => console.log('failed to connect to MongoDB', err));
 
 const port = process.env.PORT;
 app.listen(port, () => {
