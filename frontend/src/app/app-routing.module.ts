6e5a7ddfcf883e4db32d83128341ee0298059685
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-09-08 10:50:10 +0200

    responsive design added, backend configuration started

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TasksManagerComponent } from './pages/tasks-manager/tasks-manager.component';
+
+const routes: Routes = [
+  { path: '', component: TasksManagerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/app/pages/pages.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TasksManagerComponent } from './tasks-manager/tasks-manager.component';
+
+
+
+@NgModule({
+  declarations: [
+    TasksManagerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
